7b014ee3bd153eb8ee68a9383ec290fa39374def
jk <joern.kreutel@beuth-hochschule.de>
2020-04-29 00:00:13 +0200

    use improved step() impl in JWS

--- org.dieschnittstelle.ess.jws.client/src/main/java/org/dieschnittstelle/ess/jws/client/ShowTouchpointSOAPService.java
+++ org.dieschnittstelle.ess.jws.client/src/main/java/org/dieschnittstelle/ess/jws/client/ShowTouchpointSOAPService.java
@@ -1,6 +1,8 @@
 package org.dieschnittstelle.ess.jws.client;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 
 import org.apache.logging.log4j.Logger;
@@ -114,8 +116,9 @@ public class ShowTouchpointSOAPService {
 	 */
 	private static void step() {
 		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			System.out.println("/>");
-			System.in.read();
+			br.readLine();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
