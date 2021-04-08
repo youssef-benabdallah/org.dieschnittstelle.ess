5448c39d0cc0456cb0b3f2bbb99b6b4ab978c203
jk <joern.kreutel@beuth-hochschule.de>
2021-04-08 15:19:31 +0200

    more precise comment

--- org.dieschnittstelle.ess.mip.webapp/src/main/java/org/dieschnittstelle/ess/mip/webapi/RESTWebAPIRoot.java
+++ org.dieschnittstelle.ess.mip.webapp/src/main/java/org/dieschnittstelle/ess/mip/webapi/RESTWebAPIRoot.java
@@ -6,7 +6,7 @@ import javax.ws.rs.core.Application;
 import org.apache.logging.log4j.Logger;
 
 /*
- * Note that in order for the webapi to work correctly, the option
+ * Note that in order for the webapi to work correctly when running in wildfly, the option
  * -Dresteasy.preferJacksonOverJsonB=true must be set when starting
  * jboss, otherwise JsonTypeInfo will not be considered, see https://docs.jboss.org/resteasy/docs/4.3.1.Final/userguide/html/JAX-RS_2.1_additions.html
  */
