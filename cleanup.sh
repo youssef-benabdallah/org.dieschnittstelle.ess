7443259d27b5b937adb1df724d276f4df4a3488d
jk <joern.kreutel@beuth-hochschule.de>
2021-05-18 21:30:26 +0200

    modify cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -2,3 +2,5 @@ rm -R .distribution
 echo deleted .distribution folder at top level
 rm -R */.distribution
 echo deleted .distribution folders in projects
+rm -R */target
+echo deleted target folders in projects
