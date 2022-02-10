0f3ea29187681bcbdd92bb274a77cdf6434fab85
jk <joern.kreutel@beuth-hochschule.de>
2022-02-10 20:57:14 +0100

    remove all modules from the outdated JWS and ADD exercises

--- org.dieschnittstelle.ess.jws/src/main/java/org/dieschnittstelle/ess/jrs/WebAPIRoot.java
+++
@@ -1,9 +0,0 @@
-package org.dieschnittstelle.ess.jrs;
-
-import javax.ws.rs.core.Application;
-import javax.ws.rs.ApplicationPath;;
-
-@ApplicationPath("/api")
-public class WebAPIRoot extends Application {
-
-}
