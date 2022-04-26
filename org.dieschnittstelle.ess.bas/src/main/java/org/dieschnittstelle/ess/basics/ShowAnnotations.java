6bb5cc320ee8acb120496d5508c120aba9d9465f
youssef-benabdallah <youssefb019@gmail.com>
2022-04-26 18:41:44 +0200

    BAS3 done

--- org.dieschnittstelle.ess.bas/src/main/java/org/dieschnittstelle/ess/basics/ShowAnnotations.java
+++ org.dieschnittstelle.ess.bas/src/main/java/org/dieschnittstelle/ess/basics/ShowAnnotations.java
@@ -36,36 +36,47 @@ public class ShowAnnotations {
      * TODO BAS2
      */
     private static void showAttributes(Object instance) {
+
+        // TODO BAS2: create a string representation of instance by iterating
+        //  over the object's attributes / fields as provided by its class
+        //  and reading out the attribute values. The string representation
+        //  will then be built from the field names and field values.
+        //  Note that only read-access to fields via getters or direct access
+        //  is required here.
+
         Class klass = instance.getClass();
         show("class is: " + klass);
 
-		// TODO BAS2: create a string representation of instance by iterating
-		//  over the object's attributes / fields as provided by its class
-		//  and reading out the attribute values. The string representation
-		//  will then be built from the field names and field values.
-		//  Note that only read-access to fields via getters or direct access
-		//  is required here.
-		List<String> result = new ArrayList<>();
+        List<String> result = new ArrayList<>();
 
         try {
             for (Field field : klass.getDeclaredFields()) {
                 String fieldName = field.getName();
-                String firstCharUppercase = fieldName.substring(0,1).toUpperCase();
+                String firstCharUppercase = fieldName.substring(0, 1).toUpperCase();
                 String restOfName = fieldName.substring(1);
                 String fieldGetterName = "get" + firstCharUppercase + restOfName;
 
                 Method getter = klass.getDeclaredMethod(fieldGetterName);
                 String getValue = getter.invoke(instance).toString();
-                String msg = fieldName + ":" + getValue ;
+
+                // TODO BAS3: if the new @DisplayAs annotation is present on a field,
+                //  the string representation will not use the field's name, but the name
+                //  specified in the the annotation. Regardless of @DisplayAs being present
+                //  or not, the field's value will be included in the string representation.
+
+                //For BAS3
+                if (field.isAnnotationPresent(DisplayAs.class)) {
+                    fieldName = field.getAnnotation(DisplayAs.class).value();
+                }
+                String msg = fieldName + ":" + getValue;
                 result.add(msg);
             }
 
-			System.out.printf("{%s", klass.getSimpleName());
 
-			for (int i=0;i<result.size()-1;i++){
-				System.out.printf(" %s,", result.get(i) );
-			}
-			System.out.printf(" %s}", result.get(result.size()-1) );
+            for (int i = 0; i < result.size() - 1; i++) {
+                System.out.printf(" %s,", result.get(i));
+            }
+            System.out.printf(" %s}", result.get(result.size() - 1));
 
 
         } catch (NoSuchMethodException e) {
@@ -75,12 +86,10 @@ public class ShowAnnotations {
         } catch (InvocationTargetException e) {
             e.printStackTrace();
         }
-
-
-        // TODO BAS3: if the new @DisplayAs annotation is present on a field,
-        //  the string representation will not use the field's name, but the name
-        //  specified in the the annotation. Regardless of @DisplayAs being present
-        //  or not, the field's value will be included in the string representation.
     }
+
+
+
+
 
 }
